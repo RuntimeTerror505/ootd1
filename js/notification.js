7d2fcd9553453aa0f63eaf0f68afdfd3af3ddcf2
CodeJoker <126978312+RuntimeTerror505@users.noreply.github.com>
2023-10-12 09:49:51 +0000

    fix

--- js/notification.js
+++ js/notification.js
@@ -3,47 +3,41 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
     navigator.serviceWorker.register('../js/service-workers.js')
         .then(function (registration) {
             console.log('Service Worker registered with scope:', registration.scope);
-
-            // Перевірка можливості відображення сповіщень
-            if (Notification.permission === 'granted') {
-                // Показати сповіщення #1
-                showNotification("You’re now #1 in 'category name' xx");
-
-                // Показати сповіщення #2
-                showNotification("Your look 'category name' moved up in the Leaderboard <3");
-
-                // Показати сповіщення #3
-                showNotification("You’re in the Top 10 of all ootd xoxo");
-            } else if (Notification.permission !== 'denied') {
-                // Запит дозволу на відображення сповіщень
-                Notification.requestPermission()
-                    .then(function (permission) {
-                        if (permission === 'granted') {
-                            // Показати сповіщення #1
-                            showNotification("You’re now #1 in 'category name' xx");
-
-                            // Показати сповіщення #2
-                            showNotification("Your look 'category name' moved up in the Leaderboard <3");
-
-                            // Показати сповіщення #3
-                            showNotification("You’re in the Top 10 of all ootd xoxo");
-                        }
-                    });
-            }
         })
         .catch(function (error) {
             console.error('Service Worker registration failed:', error);
         });
 }
 
+// Перевірка можливості відображення сповіщень
+if ('Notification' in window && Notification.permission !== 'denied') {
+    // Запит дозволу на відображення сповіщень
+    Notification.requestPermission()
+        .then(function (permission) {
+            if (permission === 'granted') {
+                // Дозвіл отримано, тут можна відображати сповіщення
+
+                // Показати сповіщення #1
+                showNotification("You’re now #1 in 'category name' xx", registration);
+
+                // Показати сповіщення #2
+                showNotification("Your look 'category name' moved up in the Leaderboard <3");
+
+                // Показати сповіщення #3
+                showNotification("You’re in the Top 10 of all ootd xoxo");
+            }
+        });
+}
+
 // Функція для відображення сповіщень
-function showNotification(message) {
+function showNotification(message, registration) {
     if (Notification.permission === 'granted') {
         const options = {
             body: message,
-            icon: './assets/majestic.png', // Ви маєте помилку в шляху до іконки
+            icon: './assets/mahestic.png',
         };
 
-        registration.showNotification('Notification', options); // Зміни showNotification на self.registration.showNotification
+        // new Notification('Notification', options);
+        registration.showNotification('Notification', options);
     }
 }
