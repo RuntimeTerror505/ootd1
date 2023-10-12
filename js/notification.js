d4c00b8053ace565e756cc41e431d7931cf88f4f
CodeJoker <126978312+RuntimeTerror505@users.noreply.github.com>
2023-10-12 14:56:02 +0000

    fix

--- js/notification.js
+++ js/notification.js
@@ -1,24 +1,7 @@
-if ('serviceWorker' in navigator && 'PushManager' in window) {
-    navigator.serviceWorker.register('../js/service-workers.js')
-        .then(function (registration) {
-            console.log('Service Worker registered with scope:', registration.scope);
-            Notification.requestPermission().then(perm => {
-                if (perm === 'granted') {
-                    console.log(perm);
-                    showNotificationInterval("Your look 'category name' moved up in the Leaderboard <3", 10000); // Викликаємо раз в 10 секунд (10000 мілісекунд)
-                }
-            })
-        })
-        .catch(function (error) {
-            console.error('Service Worker registration failed:', error);
-        });
-}
-
-// if ('serviceWorker' in navigator) {
+// if ('serviceWorker' in navigator && 'PushManager' in window) {
 //     navigator.serviceWorker.register('../js/service-workers.js')
 //         .then(function (registration) {
 //             console.log('Service Worker registered with scope:', registration.scope);
-
 //             Notification.requestPermission().then(perm => {
 //                 if (perm === 'granted') {
 //                     console.log(perm);
@@ -30,6 +13,23 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
 //             console.error('Service Worker registration failed:', error);
 //         });
 // }
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('../js/service-workers.js')
+        .then(function (registration) {
+            console.log('Service Worker registered with scope:', registration.scope);
+
+            Notification.requestPermission().then(perm => {
+                if (perm === 'granted') {
+                    console.log(perm);
+                    showNotificationInterval("Your look 'category name' moved up in the Leaderboard <3", 10000); // Викликаємо раз в 10 секунд (10000 мілісекунд)
+                }
+            })
+        })
+        .catch(function (error) {
+            console.error('Service Worker registration failed:', error);
+        });
+}
 
 function showNotificationInterval(message, interval) {
     showNotification(message);
