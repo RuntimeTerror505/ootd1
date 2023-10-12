90e97594d1692158826299449351af8795bdbeac
CodeJoker <126978312+RuntimeTerror505@users.noreply.github.com>
2023-10-12 09:51:52 +0000

    fix

--- js/notification.js
+++ js/notification.js
@@ -1,31 +1,13 @@
-// Підключення до сервісу сповіщень (якщо необхідно)
 if ('serviceWorker' in navigator && 'PushManager' in window) {
     navigator.serviceWorker.register('../js/service-workers.js')
         .then(function (registration) {
             console.log('Service Worker registered with scope:', registration.scope);
+
+            // Показати сповіщення
+            showNotification("Your message here", registration);
         })
         .catch(function (error) {
             console.error('Service Worker registration failed:', error);
-        });
-}
-
-// Перевірка можливості відображення сповіщень
-if ('Notification' in window && Notification.permission !== 'denied') {
-    // Запит дозволу на відображення сповіщень
-    Notification.requestPermission()
-        .then(function (permission) {
-            if (permission === 'granted') {
-                // Дозвіл отримано, тут можна відображати сповіщення
-
-                // Показати сповіщення #1
-                showNotification("You’re now #1 in 'category name' xx", registration);
-
-                // Показати сповіщення #2
-                showNotification("Your look 'category name' moved up in the Leaderboard <3");
-
-                // Показати сповіщення #3
-                showNotification("You’re in the Top 10 of all ootd xoxo");
-            }
         });
 }
 
@@ -37,7 +19,6 @@ function showNotification(message, registration) {
             icon: './assets/mahestic.png',
         };
 
-        // new Notification('Notification', options);
         registration.showNotification('Notification', options);
     }
 }
