9d0716e8cb5b55099ffca1c909c17c7899ca6780
CodeJoker <126978312+RuntimeTerror505@users.noreply.github.com>
2023-10-12 14:53:37 +0000

    fix

--- js/notification.js
+++ js/notification.js
@@ -14,22 +14,22 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
         });
 }
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../js/service-workers.js')
-        .then(function (registration) {
-            console.log('Service Worker registered with scope:', registration.scope);
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('../js/service-workers.js')
+//         .then(function (registration) {
+//             console.log('Service Worker registered with scope:', registration.scope);
 
-            Notification.requestPermission().then(perm => {
-                if (perm === 'granted') {
-                    console.log(perm);
-                    showNotificationInterval("Your look 'category name' moved up in the Leaderboard <3", 10000); // Викликаємо раз в 10 секунд (10000 мілісекунд)
-                }
-            })
-        })
-        .catch(function (error) {
-            console.error('Service Worker registration failed:', error);
-        });
-}
+//             Notification.requestPermission().then(perm => {
+//                 if (perm === 'granted') {
+//                     console.log(perm);
+//                     showNotificationInterval("Your look 'category name' moved up in the Leaderboard <3", 10000); // Викликаємо раз в 10 секунд (10000 мілісекунд)
+//                 }
+//             })
+//         })
+//         .catch(function (error) {
+//             console.error('Service Worker registration failed:', error);
+//         });
+// }
 
 function showNotificationInterval(message, interval) {
     showNotification(message);
